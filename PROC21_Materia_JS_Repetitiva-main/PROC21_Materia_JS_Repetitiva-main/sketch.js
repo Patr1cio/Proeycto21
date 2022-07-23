d6b9470592454af83efd30fcc727f0567a80fbc7
Patr1cio <105463346+Patr1cio@users.noreply.github.com>
2022-07-22 20:17:38 -0500

    Add files via upload

---
+++ PROC21_Materia_JS_Repetitiva-main/PROC21_Materia_JS_Repetitiva-main/sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball
+var groundObj,leftside, rightside;
+var radius=40
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//crear los cuerpos aquí.
+var ball_option={
+	isStatic:false,
+	restitution:0.3,
+	friction:0,
+	density:1.2,
+}
+ball = Bodies.circle(260, 100, radius/2, ball_option)
+World.add (world, ball)
+groundObj= new Ground(width/2, 670, width, 20);
+leftside= new Ground(1100, 600, 20, 120);
+rightside= new Ground(1350, 600, 20, 120);
+Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  groundObj.display();
+  leftside.display();
+  rightside.display();
+  ellipse(ball.position.x, ball.position.y, radius, radius)
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if (keyCode===UP_ARROW){
+		Matter.Body.applyForce(ball, ball.position, {x:85, y:-85})
+	}
+}
+
+
